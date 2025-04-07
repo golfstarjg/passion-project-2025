99ca75aae6089b0935a080c624565fa1b89c864b
Jonah Gross <206627068+golfstarjg@users.noreply.github.com>
2025-04-07 19:04:13 -0400

    Create rockPaperScissorsShoot.py
    This is a rock paper scissors game where you go against the computer.

---
+++ rockPaperScissorsShoot.py
@@ -0,0 +1,45 @@
+import random
+#random.seed()
+
+
+while True:
+
+  computerChoice = random.randint(1,3)
+
+  choice = input("Rock, Paper Scissors, Shoot! Choose rock, paper or scissors. 1 = rock, 2 = scissors, 3 = paper 4 = done      ")
+  choice = int(choice)
+  if choice == 1:
+    print("You chose rock, lets see what the computer chose")
+  elif choice == 2:
+    print("You chose scissors, lets see what the computer chose")
+  elif choice == 3:
+    print("You chose paper, lets see what the computer chose")
+  
+  if computerChoice == 1:
+    print("The Computer's Choice is rock, lets see who wins")
+  elif computerChoice == 2:
+    print("The Computer's Choice is scissors, lets see who wins")
+  elif computerChoice == 3:
+    print("The Computer's Choice is paper, lets see who wins")
+  
+  if computerChoice == choice:
+    print("It was a tie")
+  elif computerChoice == 1 and choice == 2:
+    print("The Computer wins") 
+  elif computerChoice == 1 and choice == 3:
+    print("You win")
+  elif computerChoice == 2 and choice == 1:
+    print("You win")
+  elif computerChoice == choice:
+    print("It was a tie")
+  elif computerChoice == 2 and choice == 3:
+    print("The Computer wins")
+  elif computerChoice == 3 and choice == 1:
+    print("The Computer wins")
+  elif computerChoice == 3 and choice == 2:
+    print("You win")
+  elif computerChoice == choice:
+    print("It was a tie")
+  elif choice == 4:
+    break
+  
